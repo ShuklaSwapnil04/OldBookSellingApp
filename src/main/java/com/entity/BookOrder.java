5a572457b27b537877d0f039bb96eec7a7caaf62
Swapnil Shukla <54302125+ShuklaSwapnil04@users.noreply.github.com>
2021-08-04 04:20:03 -0700

    bookorder

---
+++ src/main/java/com/entity/BookOrder.java
@@ -0,0 +1,89 @@
+package com.entity;
+
+public class BookOrder {
+	
+	private int id;
+	private String orderId;
+	private String username;
+	private String email;
+	private String phno;
+	private String fulladdress;
+	
+	private String bookName;
+	private String author;
+	private String price;
+	
+	
+    public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	private String paymentType;
+	public BookOrder() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public String getFulladdress() {
+		return fulladdress;
+	}
+	public void setFulladdress(String fulladdress) {
+		this.fulladdress = fulladdress;
+	}
+	public String getPaymentType() {
+		return paymentType;
+	}
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+	@Override
+	public String toString() {
+		return "BookOrder [id=" + id + ", username=" + username + ", email=" + email + ", phno=" + phno
+				+ ", fulladdress=" + fulladdress + ", paymentType=" + paymentType + "]";
+	}
+    
+
+}
