b5ee56b32bcaec002d7cc1ff227738f9f74cf3b3
Swapnil Shukla <54302125+ShuklaSwapnil04@users.noreply.github.com>
2021-08-12 04:17:57 -0700

    cart

---
+++ src/main/java/com/user/servlet/CartServlet.java
@@ -0,0 +1,64 @@
+package com.user.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.DAO.BookDAOImpl;
+import com.DAO.CartDAOImpl;
+import com.DB.DBConnect;
+import com.entity.BookDtls;
+import com.entity.Cart;
+@WebServlet("/cart")
+public class CartServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		try {
+			int bid=Integer.parseInt(req.getParameter("bid"));
+	  		int uid=Integer.parseInt(req.getParameter("uid"));
+			
+			BookDAOImpl dao=new BookDAOImpl(DBConnect.getConn());
+			BookDtls b=dao.getBookById(bid);
+			
+			Cart c=new Cart();
+			c.setBid(bid);
+			c.setUserId(uid);
+			c.setBookName(b.getBookname());
+			c.setAuthor(b.getAuthor());
+			c.setPrice(Double.parseDouble(b.getPrice()));
+			c.setTotal_price(Double.parseDouble(b.getPrice()));
+	
+			CartDAOImpl dao2 =new CartDAOImpl(DBConnect.getConn());
+			boolean f=dao2.addToCart(c);
+			
+			HttpSession session=req.getSession();
+			
+			if(f)
+			{
+				session.setAttribute("addcart","Book addded to cart");
+				resp.sendRedirect("all_new_book.jsp");
+				System.out.println("Success");
+			}
+			else
+			{
+				session.setAttribute("failed","Something went wrong");
+				System.out.println("fail");
+			}
+			
+		} catch (Exception e) {
+		e.printStackTrace();
+			// TODO: handle exception
+		}
+	}
+
+
+}
